b801a6e539397e13dfdf5f1d6be2af3b777531e3
1nflktd <henscalon@hotmail.com>
2014-10-30 21:58:19 -0200

    Update 204 - Firetruck.c

--- 204 - Firetruck.c
+++ 204 - Firetruck.c
@@ -1,86 +1,79 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-
-struct par {
-	int x, y;
-};
+#include <cstdlib>
+#include <iostream>
+#include <string>
+#include <cstdio>
+#include <sstream>
 
 int matriz[22][22];
 int visitado[22];
 
-void fireback(int inicio, int fim, char res[], int idres)
+void fireback(int inicio, int fim, std::string res)
 {
-	visitado[inicio] = 1;
-	/*if (inicio > 9)
-	{
-		int x = inicio;
-		char b[1], b2[1];
-		sprintf(b, "%d", inicio/10);
-		res[idres++] = (char)b;
-		sprintf(b2, "%d", x % 10);
-		res[idres] = (char)b2;
-	}
-	else 
-	{
-		char buffer[1];
-		sprintf(buffer, "%d", inicio);
-		res[idres] = (char)buffer;
-	}
-	/*strcat(res, buffer);
-	strcat(res, " ");*/
-
-	if (inicio == fim)
-	{
-		res = res + '\0';
-		printf("%s\n", res);
-		visitado[inicio] = 0;
-		return;
-	}
-
-	for (int i = 1; i <= 21; i++)
-	{
-		if (visitado[i] == 0)
-		{
-			if (matriz[inicio][i])
-			{
-				fireback(i, fim, res + '1', idres++);
-				continue;
-			}
-			if (matriz[i][inicio])
-			{
-				fireback(i, fim, res + '1', idres++);
-			}
-		}
-	}
-	visitado[inicio] = 0;
+    int i;
+    visitado[inicio] = 1;
+    
+    if (inicio > 1)
+    {
+        std::stringstream sstm;
+        sstm << res << " " << inicio;
+        res = sstm.str();
+    }
+    else
+    {
+        std::stringstream sstm;
+        sstm << res << inicio;
+        res = sstm.str();
+    }
+    
+    if (inicio == fim)
+    {
+        std::cout << res << '\n';
+        visitado[inicio] = 0;
+        return;
+    }
+    
+    for (i = 1; i <= 21; i++)
+    {
+        if (!visitado[i])
+        {
+            if(matriz[inicio][i])
+            {
+                fireback(i, fim, res);
+                continue;
+            }
+            if (matriz[i][inicio])
+            {
+                fireback(i, fim, res);
+            }
+        }
+    }
+    visitado[inicio] = 0;
 }
 
 int main()
 {
-	int streetcorners, x, y, i;
+    int streetcorners, x, y, i;
+    std::string res;
+    
+    std::cin >> streetcorners;
+    
+    for (i = 1; i <= 21; i++)
+    {
+        visitado[i] = 0;
+        for (int j = 1; j <= 21; j++)
+        {
+            matriz[i][j] = 0;
+        }
+    }
+    
+    do
+    {
+        std::cin >> x >> y;
+        matriz[x][y] = 1;
+    }
+    while (x != 0 && y != 0);
 
-	scanf("%d", &streetcorners);
+    fireback(1, streetcorners, res);
 
-	for (int i = 1; i <= 21; i++)
-	{
-		visitado[i] = 0;
-		for (int j = 1; j <= 21; j++)
-		{
-			matriz[i][j] = 0;
-		}
-	}
-
-	do
-	{
-		scanf("%d %d", &x, &y);
-		matriz[x][y] = 1;
-	} while (x != 0 && y != 0);
-
-	char res[42];
-	strcpy(res, "");
-	fireback(1, streetcorners, res, 0);
-
-	system("pause");
-	return 0;
+    return 0;
 }
