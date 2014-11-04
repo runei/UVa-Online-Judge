a6ee0340081ab2484a522ce2bb96503b7b8279d8
Leonardo Invernizzi <leo.invernizzi@hotmail.com>
2014-11-03 22:12:27 -0200

    Time Limit Exceeded

---
+++ 11195 - Another n-Queen Problem.cpp
@@ -0,0 +1,74 @@
+#include <cstdio>
+#include <iostream>
+
+#define TAM 15
+
+using namespace std;
+
+int n;
+int qtde;
+int colQueen[TAM];
+int rowQueen[TAM];
+int board[TAM][TAM];
+
+bool valida(int col, int row)
+{
+	int i;
+	if (board[col][row] == '*' || rowQueen[row])
+	{
+		return false;
+	}
+	for (i = 0; i < col; i++)
+	{
+		if (colQueen[i] - i == row - col ||
+			colQueen[i] + i == row + col)
+		{
+			return false;
+		}
+	}
+	return true;
+}
+
+void queenBack(int col)
+{
+	int i;
+	if (col == n)
+	{
+		qtde++;
+		return;
+	}
+	for (i = 0; i < n; i++)
+	{
+		if (valida(col, i))
+		{
+			colQueen[col] = i;
+			rowQueen[i] = 1;
+			queenBack(col + 1);
+			colQueen[col] = 0;
+			rowQueen[i] = 0;
+		}
+	}
+}
+
+int main()
+{
+	int i, j;
+	int nroCasos = 1;
+	while (scanf("%d", &n) != EOF && n != 0)
+	{
+		qtde = 0;
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < n; j++)
+			{
+				scanf(" %c", &board[i][j]);
+			}
+		}
+
+		queenBack(0);
+
+		printf("Case %d: %d\n", nroCasos, qtde);
+
+	}
+	return 0;
+}
